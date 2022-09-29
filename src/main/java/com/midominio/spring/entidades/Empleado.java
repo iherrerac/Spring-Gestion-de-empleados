3496c882680f0b43c6f4fff399e4ed03f6eb60cc
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-09-29 13:32:18 +0200

    Spring Data JPA: Modificamos entidad "productos"

--- src/main/java/com/midominio/spring/entidades/Empleado.java
+++ src/main/java/com/midominio/spring/entidades/Empleado.java
@@ -2,14 +2,23 @@ package com.midominio.spring.entidades;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Empleado {
+	
+	@Id @GeneratedValue
 	@Min(value=0,message="{empleado.id.mayorquecero}")
 	private long id;
+	
+	@Column(nullable=false)
 	@NotNull (message="aaaaaaaa")
 	private String nombre;
 	@Email (message="{empleado.email.email}")
