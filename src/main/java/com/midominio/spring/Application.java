6d3f3377651cae02eab82d1c216aa37dd561858e
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-10-04 10:12:44 +0200

    Spring: Spring Data JPA. Rellenamos la BD con un listado de nombres  y apellidos.  modificamos codigos avatars en list.html para que se generen con  el email

--- src/main/java/com/midominio/spring/Application.java
+++ src/main/java/com/midominio/spring/Application.java
@@ -1,11 +1,15 @@
 package com.midominio.spring;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.midominio.spring.entidades.Empleado;
@@ -48,11 +52,36 @@ CommandLineRunner initData(EmpleadoRepository repositorio) {
 //		
 //		repositorio.findAll().forEach(System.out::println);
 
-		repositorio.saveAll(
-				Arrays.asList(new Empleado(1, "Antonio García", "antonio.garcia@openwebinars.net", "954000000", true),
-						new Empleado(2, "María López", "maria.lopez@openwebinars.net", "954000000",false),
-						new Empleado(3, "Ángel Antúnez", "angel.antunez@openwebinars.net", "954000000", true)));
+//		repositorio.saveAll(
+//				Arrays.asList(new Empleado(1, "Antonio García", "antonio.garcia@openwebinars.net", "954000000", true),
+//						new Empleado(2, "María López", "maria.lopez@openwebinars.net", "954000000",false),
+//						new Empleado(3, "Ángel Antúnez", "angel.antunez@openwebinars.net", "954000000", true)));
+//
+//	};
+		
+		List<String> nombres = Arrays.asList("Lucas", "Hugo", "Martín", "Daniel", "Pablo", "Alejandro", "Mateo",
+				"Adrián", "Álvaro", "Manuel", "Leo", "David", "Mario", "Diego", "Javier", "Luis", "Marcos", "Juan",
+				"José", "Gonzalo", "Lucía", "Sofía", "María", "Martina", "Paula", "Julia", "Daniela", "Valeria",
+				"Alba", "Emma", "Carla", "Sara", "Noa", "Carmen", "Claudia", "Valentina", "Alma", "Ana", "Luisa",
+				"Marta");
+
+		List<String> apellidos = Arrays.asList("García", "González", "López", "Rodríguez", "Martínez", "Sánchez",
+				"Pérez", "Gómez", "Martín", "Saez", "Velasco", "Moya", "Soler", "Parra", "Bravo", "Rojas", "Romero",
+				"Sosa", "Torres", "Álvarez", "Flores", "Molina", "Ortiz", "Silva", "Torres");
+
+
+		
+		Collections.shuffle(nombres); //Barajamos nombres
+
+		repositorio.saveAll(IntStream.rangeClosed(1, nombres.size()).mapToObj((i) -> {//Recorremos todo el array de nombres
+			String nombre = nombres.get(i-1);
+			String apellido1 = apellidos.get(ThreadLocalRandom.current().nextInt(apellidos.size())); //Apellidos aleatorio
+			String apellido2 = apellidos.get(ThreadLocalRandom.current().nextInt(apellidos.size()));
+			return new Empleado(String.format("%s %s %s", nombre, apellido1, apellido2), 
+					String.format("%s.%s@openwebinars.net", nombre.toLowerCase(), apellido1.toLowerCase()), "954000000");
+		}).collect(Collectors.toList())); //Lo pasamos con collect a una lista y que se grabará con saveAll
 
 	};
+	
 }
 }
